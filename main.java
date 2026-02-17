47cf88eadd7efd6fc499ff8f793fc7d7f8594f40
ceo63arul-pixel <ceo63arul@gmail.com>
2026-02-17 15:14:30 +0530

    Rename Main class to OOPSBannerApp and update output

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
-public class Main {
+public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub!");
+        System.out.println("OOPS");
     }
 }
+
